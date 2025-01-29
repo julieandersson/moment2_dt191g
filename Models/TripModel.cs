e0728c04315dc4d543af298b328c66937cfcd62d
julieandersson <juan2301@student.miun.se>
2025-01-29 15:00:45 +0200

    försöker lista ut varför errormessage i span ej fungerar för start- och slutdatum samt rating, tar bort errormessage sålänge och behåller bara stringlength och minlength samt sätter type i formuläret

--- Models/TripModel.cs
+++ Models/TripModel.cs
@@ -5,23 +5,25 @@ namespace moment2_dt191g.Models;
 public class TripModel {
     // Properties
 
-    [Required(ErrorMessage = "Land måste anges.")]
-    [StringLength(56, MinimumLength = 2, ErrorMessage = "Landet måste vara mellan 2 och 56 tecken.")]
+    [Required]
+    [StringLength(56, MinimumLength = 2)]
     public required string Country { get; set; } // Landet man besökt
     
-    [Required(ErrorMessage = "Stad måste anges.")]
-    [StringLength(85, MinimumLength = 1, ErrorMessage = "Stadens namn måste vara mellan 1 och 85 tecken.")]
+    [Required]
+    [StringLength(85, MinimumLength = 1)]
     public required string City { get; set; } // Staden man besökte
     
-    [Required(ErrorMessage = "Startdatum måste anges.")]
-    [DataType(DataType.Date, ErrorMessage = "Startdatum måste vara ett giltigt datum.")]
+    [Required]
+    [DataType(DataType.Date)]
     public required DateTime StartDate { get; set; } // När resan startade
     
-    [Required(ErrorMessage = "Slutdatum måste anges.")]
-    [DataType(DataType.Date, ErrorMessage = "Slutdatum måste vara ett giltigt datum.")]
+    [Required]
+    [DataType(DataType.Date)]
     public required DateTime EndDate { get; set; } // När resan slutade
     
-    [Required(ErrorMessage = "Betyg måste anges.")]
-    [Range(1, 5, ErrorMessage = "Betyg måste vara mellan 1 och 5.")]
+    [Required]
+    [Range(1, 5)]
     public required int Rating { get; set; } // Betyg 1-5
 }
+
+
