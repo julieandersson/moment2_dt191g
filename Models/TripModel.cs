3348692a0f8563f16ac3c40b1c6fca464ea52201
julieandersson <juan2301@student.miun.se>
2025-01-29 11:32:44 +0200

    skapar formulär för att lägga till en ny resa, sätter required i modellen för att validera input och kräva att varje fält är ifyllt

--- Models/TripModel.cs
+++ Models/TripModel.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace moment2_dt191g.Models;
 
 public class TripModel {
     // Properties
+
+    [Required]
     public required string Country { get; set; } // Landet man besökt
+    
+    [Required]
     public required string City { get; set; } // Staden man besökte
+    
+    [Required]
     public DateTime StartDate { get; set; } // När resan startade
+    
+    [Required]
     public DateTime EndDate { get; set; } // När resan slutade
+    
+    [Required]
     public required int Rating { get; set; } // Betyg 1-5
 }
